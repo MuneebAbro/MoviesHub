36eb39ef3b58817be4879f9fcba083b8ec6ffee7
Muneeb Abro <muneebabr000@gmail.com>
2025-04-13 20:19:43 +0500

    Top Bar added

---
+++ src/components/homeScreen/TopBar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+const TopBar = () => {
+  return (
+    <View>
+         <View style={styles.header}>
+           <TouchableOpacity>
+             <Icon name="menu" size={28} color="#fff" />
+           </TouchableOpacity>
+   
+           <Text style={styles.title}>MovieHub</Text>
+   
+           <TouchableOpacity>
+             <Icon name="search" size={28} color="#fff" />
+           </TouchableOpacity>
+         </View>
+    </View>
+  )
+}
+
+export default TopBar
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingTop: 8,
+      paddingHorizontal: 20,
+      paddingBottom: 10,
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#fff',
+    },
+   
+  });
+  
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import TopBar from '../components/homeScreen/TopBar';
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity>
-          <Icon name="menu" size={28} color="#fff" />
-        </TouchableOpacity>
+   
+        <View style={styles.container}>
 
-        <Text style={styles.title}>MovieHub</Text>
-
-        <TouchableOpacity>
-          <Icon name="search" size={28} color="#fff" />
-        </TouchableOpacity>
-      </View>
+          <TopBar/>
 
       <View style={styles.content}>
         <Text style={{ color: '#fff' }}>HomeScreen</Text>
       </View>
-    </View>
+      </View>
   );
 };
 
@@ -30,19 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingTop: 50,
-    paddingHorizontal: 20,
-    paddingBottom: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
   },
   content: {
     flex: 1,
